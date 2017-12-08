03a0f4d5f02413c0deb09bb65588b8ad791d382e
Roman Schulte-Sasse <sasse@molgen.mpg.de>
2017-12-08 11:54:07 +0100

    Less printing and complete network generation.
    Still no good pipeline though.

--- generate_network.py
+++ generate_network.py
@@ -33,25 +33,6 @@ def parse_args():
     args = parser.parse_args()
     return args.node_num, args.min_edge_num, args.subnet_path, args.out_path
 
-def get_random_neighbor(G, position):
-    all_nbs = nx.neighbors(G, position)
-    return random.choice(all_nbs)
-
-
-def _implant_rec(to_assign, G, G_m, pos, mapping):
-    if len(to_assign) is 0:
-        return mapping
-    pos_gm = nodes_sub[0]
-    mapping[pos] = pos_gm
-    nbs_gm = [i for i in nx.all_neighbors(G_m, pos_gm) if i in to_assign]
-    nbs_g = nx.all_neighbors(G, pos)
-    # TODO: Check if nbs_gm is longer than nbs_g and add nodes to g until same size
-    next_nb = 0
-    for node_gm in nbs_gm:
-        _implant_rec(to_assign[1:], G, G_m, nbs_g[next_nb], mapping)
-        next_nb += 1
-
-
 def _get_neighbors(G, pos, closed_list):
     return [i for i in nx.all_neighbors(G, pos) if not i in closed_list]
 
@@ -63,7 +44,7 @@ def _add_edges_if_required(G, pos_g, neighbors_G, neighbors_sub, next_node_count
         for i in range(diff):
             G.add_node(next_node_count)
             G.add_edge(pos_g, next_node_count)
-            print ("new edge: {} -> {}".format(pos_g, next_node_count))
+            #print ("new edge: {} -> {}".format(pos_g, next_node_count))
             next_node_count += 1
     return G, next_node_count, changed
 
@@ -78,7 +59,6 @@ def calculate_node_mapping(G, G_m, position):
     next_node_name = nx.number_of_nodes(G)
     pos_gm = list(G_m.nodes())[0]
     pos_g = position
-    print (pos_gm, pos_g)
     mapping = {pos_gm: pos_g}
     to_explore = [pos_gm]
     mapped_g = [pos_g]
@@ -134,7 +114,7 @@ def implant_motifs(G, subnetworks):
     implant_positions = []
     for sub_idx in range(len(subnetworks)):
         position = random.randrange(0, nx.number_of_nodes(G))
-        print ("sub_idx: {}\tposition: {}".format(sub_idx, position))
+        print ("Implanting network {} at position: {}".format(sub_idx, position))
         G = implant_single_motif(G, subnetworks[sub_idx], position)
         implant_positions.append(position)
     return G, implant_positions
@@ -151,27 +131,31 @@ def generate_network(node_num, min_edge_num, subnetworks):
     desired subnetwork topology.
     """
     # Create random network.
+    print ("Creating Network...")
     for i in range(1, node_num):
         G = nx.powerlaw_cluster_graph(node_num, i, 0.8, seed=42)
         if nx.number_of_edges(G) > min_edge_num:
             break
 
+    # implant motifs and return
+    print ("Network built! Implanting {} subnetworks...".format(len(subnetworks)))
+    G, positions = implant_motifs(G, subnetworks)
+
     # plot node degree distribution
-    degrees = pd.Series(G.degree())
+    print ("Implanted subnetworks! Plotting node degree distribution...")
+    degrees = np.array([v for k, v in list(G.degree())])
     fig = plt.figure(figsize=(14, 8))
-    plt.hist(degrees, bins=np.arange(1, 100, 1))
+    plt.hist(degrees, bins=np.arange(0, 100, 1))
     fig.savefig('random_network_distribution.png')
 
-    # output some statistics
-    print ("Nodes: {}".format(len(G.nodes())))
-    print ("Edges: {}".format(len(G.edges())))
-
-    # implant motifs and return
-    return implant_motifs(G, subnet_path)
+    # output some statistics and return
+    print ("Done! Created network with {} nodes and {} edges".format(nx.number_of_nodes(G),
+                                                                     nx.number_of_edges(G))
+           )
+    return G, positions
 
 if __name__ == "__main__":
-    #node_num, min_edge_num, subnet_path, out_path = parse_args()
-    #generate_network(node_num=node_num, min_edge_num=min_edge_num)
+    node_num, min_edge_num, subnet_path, out_path = parse_args()
     subnetworks = []
     # star
     A1 = np.array([[0,1,1,1],
@@ -195,7 +179,6 @@ if __name__ == "__main__":
                    [1,1,0,1],
                    [1,1,1,0]])
     subnetworks.append(nx.from_numpy_matrix(A3))
-
     # network
     A = np.array([[0,1,0,0,0,0,0,0],
                   [1,0,1,1,0,0,0,0],
@@ -206,7 +189,7 @@ if __name__ == "__main__":
                   [0,0,0,0,1,0,0,0],
                   [0,0,0,1,0,0,0,0]])
     G, positions = implant_motifs(nx.from_numpy_matrix(A), subnetworks)
-    print (G.nodes())
-    print (G.edges())
-    print (positions)
-    #nx.draw(G)
+    #G, positions = generate_network(node_num, min_edge_num, subnetworks)
+
+    nx.draw(G, with_labels=True)
+    plt.savefig('network.png')
